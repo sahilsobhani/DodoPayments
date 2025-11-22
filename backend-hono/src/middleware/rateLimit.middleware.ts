a80a9d6e6788eead485ca542f9054e0c5b9d186e
Sahil Sobhani <92987477+sahilsobhani@users.noreply.github.com>
2025-11-22 18:54:41 +0530

    Refactor rate limiting middleware for IP extraction
    Refactored rate limiting middleware to extract client IP using a dedicated function.

--- backend-hono/src/middleware/rateLimit.middleware.ts
+++ backend-hono/src/middleware/rateLimit.middleware.ts
@@ -1,15 +1,30 @@
 import { rateLimiter } from "hono-rate-limiter";
+import { getConnInfo } from "hono/vercel";
+import type { Context } from 'hono'
 
-import type { MiddlewareHandler } from 'hono'
-  
-export const rateLimit: MiddlewareHandler = rateLimiter({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    limit: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes).
-    standardHeaders: "draft-6", 
-    keyGenerator: (c) => c.req.header("x-forwarded-for") || "local",
-    message: { status: 'error', echo: 'Rate Limit has been reached, please try again later.' }, //custom json messsage to be sent back
-  })
+// Function to extract the client's IP address from the request contexts
+function getClientIP(c: Context ) {
+  const info = getConnInfo(c);
 
+  return (
+    info.remote?.address ||
+    c.req.header("x-vercel-forwarded-for") ||
+    c.req.header("x-vercel-proxy-ip") ||
+    c.req.header("cf-connecting-ip") ||
+    c.req.header("x-real-ip") ||
+    c.req.header("x-forwarded-for")?.split(",")[0].trim() ||
+    "local"
+  );
+}
 
-
-
+// Rate limiting middleware configuration
+export const rateLimit = rateLimiter({
+  windowMs: 60_000,
+  limit: 5,
+  standardHeaders: "draft-6",
+  keyGenerator: (c) => getClientIP(c),
+  message: {
+    status: "error",
+    echo: "Rate Limit has been reached, please try again later.",
+  },
+});
