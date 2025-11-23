70744f04c4439241691e2251b855f488be328775
Sahil Sobhani <sahilsobhani4@gmail.com>
2025-11-23 09:29:44 +0530

    refactored page.tsx to be extensible

---
+++ frontend-nextjs/app/components/dashboard.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+//Sidebar & Header
+import { AppSidebar } from "@/app/components/app-sidebar";
+import { DashboardHeader } from "@/app/components/dashboard-header";
+
+// Cards
+import MyCards from "@/app/components/cards/my-cards";
+import RecentTransactions from "@/app/components/cards/recent-transactions";
+import SpendingSummary from "@/app/components/cards/spending-summary";
+import MySubscriptions from "@/app/components/cards/my-subscriptions";
+import TotalExpenses from "@/app/components/cards/total-expenses";
+import Exchange from "@/app/components/cards/exchange";
+import CreditScore from "@/app/components/cards/credit-score";
+
+export default function DashboardLayout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+
+      <main className="flex-1 flex flex-col overflow-hidden md:overflow-clip">
+        <header className="h-14 flex items-center px-4 my-2 shrink-0">
+          <DashboardHeader />
+        </header>
+
+        <div className="grid gap-6 px-4 md:px-6 py-3 grid-cols-1 md:grid-cols-3 xl:grid-cols-3 md:h-[calc(100vh-5.5rem)] md:overflow-clip">
+          {/* COLUMN 1 */}
+          <div className="flex flex-col gap-4 md:flex-1 md:min-h-0">
+            <div className="md:flex-1 md:min-h-0 md:flex"><MyCards /></div>
+            <div className="md:flex-1 md:min-h-0 md:flex"><RecentTransactions /></div>
+          </div>
+
+          {/* COLUMN 2 */}
+          <div className="flex flex-col gap-4 md:flex-1 md:min-h-0">
+            <div className="md:flex-1 md:min-h-0 md:flex"><SpendingSummary /></div>
+            <div className="md:flex-1 md:min-h-0 md:flex"><MySubscriptions /></div>
+          </div>
+
+          {/* COLUMN 3 */}
+          <div className="flex flex-col gap-4 md:flex-1 md:min-h-0">
+            <div className="md:flex-1 md:min-h-0 md:flex"><TotalExpenses /></div>
+            <div className="md:flex-2 md:min-h-0 md:flex"><Exchange /></div>
+            <div className="md:flex-1 md:min-h-0 md:flex"><CreditScore /></div>
+          </div>
+        </div>
+      </main>
+    </SidebarProvider>
+  );
+}
